f555a2f162dbd05b6ff832c37da997541ca06bd8
Liza <elizavetamihel9@gmail.com>
2025-06-05 15:24:45 +0300

    Allure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 [![Java CI with Gradle](https://github.com/ElizavetaMi/DS5/actions/workflows/gradle.yml/badge.svg)](https://github.com/ElizavetaMi/DS5/actions/workflows/gradle.yml)
+
+![img.png](img.png)
--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,4 +1,8 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -12,6 +16,16 @@ public class CardDeliveryTest {
     @BeforeEach
     void setUp() {
         open("http://localhost:9999");
+    }
+
+    @BeforeAll
+    static void setUpAll () {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll () {
+        SelenideLogger.removeListener("allure");
     }
 
     @Test
